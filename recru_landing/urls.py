90e7fcc8ec3ccc3e2a0c15807617878c3b75c460
johncarlo-vss <jcvillas.vss@gmail.com>
2022-04-13 14:17:14 -0700

    Minor changes on little disputes

--- recru_landing/urls.py
+++ recru_landing/urls.py
@@ -5,11 +5,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
     path('job_offers/', views.job_offer, name='job_offers'),
-=======
     path('job_offer/', views.job_offer, name='job_offer'),
->>>>>>> e2bf99961c37d5aa4a93e3d867a4b713c9209768
 ]
 
 urlpatterns += staticfiles_urlpatterns()
