b40448e1efd304f1f30eea471682e9e960df947a
joelito-vss <jcaorte.vss@gmail.com>
2022-04-13 09:33:54 -0700

    Added job-offers

--- recru_landing/urls.py
+++ recru_landing/urls.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('job_offer/' views.job_offer, name='job_offer'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
